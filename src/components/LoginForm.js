8b27a2c7e08bd4d8f147a205c41fe6a8044a1e6d
Adam Mackintosh <agm1984@gmail.com>
2017-10-02 23:43:15 -0700

    added some work

---
+++ src/components/LoginForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { Button, Card, CardSection, Input, LoadingSpinner } from './common'
+import firebase from 'firebase'
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            error: '',
+            loading: false
+        }
+    }
+
+    handleLogin() {
+        const { email, password } = this.state
+
+        this.setState({ error: '', loading: true })
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication failed.' })
+                    })
+            })
+    }
+
+    renderButton() {
+        if (this.state.loading) return <LoadingSpinner size="small" />
+
+        return (
+            <Button onPress={() => this.handleLogin()}>
+                Login
+            </Button>
+        )
+    }
+
+    render() {
+        const { errorTextStyle } = styles
+
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="eg: user@gmail.com"
+                        onChangeText={(email) => this.setState({ email })}
+                        value={this.state.email}
+                        autoCorrect={false}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Password"
+                        placeholder="password"
+                        onChangeText={(password) => this.setState({ password })}
+                        value={this.state.password}
+                        autoCorrect={false}
+                        secureTextEntry
+                    />
+                </CardSection>
+
+                <Text style={errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
+                <CardSection>
+                    {this.renderButton()}
+                </CardSection>
+            </Card>
+        )
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
+
+export default LoginForm
